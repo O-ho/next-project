69efb99051e259b1a3089b461b92ec6428a62a80
blockodyssey-yhkim <yhkim@blockodyssey.io>
2023-08-17 19:11:03 +0900

    theme

---
+++ src/styles/Theme/theme.ts
@@ -0,0 +1,24 @@
+export const theme = {
+    colors: {
+        primary: '#0070f3',
+        danger: '#ff0000',
+        success: '#00ff00',
+        warning: '#ffff00',
+        info: '#00ffff',
+        white: '#ffffff',
+        black: '#000000',
+        gray: '#cccccc',
+        lightGray: '#f1f1f1',
+        darkGray: '#333333',
+    },
+    padding: {
+        small: '8px',
+        middle: '16px',
+        large: '24px',
+    },
+    margin: {
+        small: '8px',
+        middle: '16px',
+        large: '24px',
+    },
+};
